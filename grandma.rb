f85945ea296b75be0e24ec799625df8b90fa6f6b
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-19 21:44:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-  if all capitals
+def speak_to_grandma(speak)
+  if speak.upcase
     puts "NO, NOT SINCE 1938"
-  elsif "I LOVE YOU GRANDMA"
+  elsif speak == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
